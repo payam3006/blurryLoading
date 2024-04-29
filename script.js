bff308449c89e0fb5214b3c07a873814969532b9
Payam3006 <payam3006@gmail.com>
2024-04-30 00:20:05 +0330

    run after page load

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const mainBlur = document.getElementById("main");
 const loadingPercent = document.getElementById("loadingPercent");
 let ii = 1;
-const start = setInterval(function () {
-  mainBlur.style.filter = `blur(${10 - ii / 10}px)`;
-  loadingPercent.style.opacity = 1 - ii / 100;
-  loadingPercent.innerText = `${ii}%`;
-  ii += 1;
-  if (ii == 101) {
-    clearInterval(start);
-  }
-}, 20);
 
-// start();
+const start = () => {
+  const start = setInterval(function () {
+    mainBlur.style.filter = `blur(${10 - ii / 10}px)`;
+    loadingPercent.style.opacity = 1 - ii / 100;
+    loadingPercent.innerText = `${ii}%`;
+    ii += 1;
+    if (ii == 101) {
+      clearInterval(start);
+    }
+  }, 20);
+};
 
+window.addEventListener("load", start());
 // for (let i = 1; i < 1000; i++) {
 //   setTimeout(function () {
 //     mainBlur.style.filter = `blur(${10 - i / 10}px)`;
